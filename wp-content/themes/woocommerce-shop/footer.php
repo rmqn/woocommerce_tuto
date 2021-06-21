a46750cf16ba7326067a3b11ef606507817361fc
rmqn <seeeeek@hotmail.fr>
2021-06-21 17:41:43 +0200

    first

---
+++ wp-content/themes/woocommerce-shop/footer.php
@@ -0,0 +1,56 @@
+<footer>
+        <div class="footer">
+            <div class="footer-container">
+                <h3 class="footer-container__heading">How can we help? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></h3>
+                <?php
+                    wp_nav_menu(array(
+                        'menu' => '18',
+                        'menu_class' => 'footer-container__body'
+                    ))
+                ?>
+            </div>
+            <div class="footer-container">
+                <h3 class="footer-container__heading">Useful Info <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></h3>
+                <?php
+                    wp_nav_menu(array(
+                        'menu' => '18',
+                        'menu_class' => 'footer-container__body'
+                    ))
+                ?>
+            </div>
+            <div class="footer-container">
+                <h3 class="footer-container__heading">Company <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></h3>
+                <ul class="footer-container__body">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Security</a></li>
+                </ul>
+            </div>
+            <div class="footer-container">
+                <h3 class="footer-container__heading">Download <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></h3>
+                <p class="footer-container__body">
+                    <img src="images/appstore.png">
+                </p>
+            </div>
+        </div>
+
+        <div class="copyright">
+            <div class="copyright__social">
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-youtube"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>
+                </a>
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
+                </a>
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+                </a>
+            </div>
+
+            <div class="copyright__body">&copy; <?php echo date("Y"); echo ' '; echo bloginfo('name'); ?> </div>
+        </div> 
+    </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/woocommerce-shop/js/scripts.js
@@ -0,0 +1,8 @@
+  jQuery( document ).ready(function() {
+
+    jQuery( '.menu-link' ).click(function(){
+        jQuery( '.mobile-menu' ).toggle("slow", function(){ });
+    });
+
+
+});
